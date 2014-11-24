f6eb6da7057e16964347242fb01fe7a0f74f34b1
Bob de Graaf <graafict@gmail.com>
2014-11-24 10:49:54 +0100

    Updated NSObject categories with great properties/value methods

--- NSObject+Helper.h
+++ NSObject+Helper.h
@@ -1,14 +1,18 @@
 //
 //  NSObject+Helper.h
+//  TVGiDS.tv 1.0
 //
-//  Created by Bob de Graaf on 01-02-14.
-//  Copyright (c) 2014 GraafICT. All rights reserved.
+//  Created by Bob de Graaf on 08-07-13.
+//  Copyright (c) 2013 MobilePioneers. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSObject (Helper)
 
+-(id)initWithDictionarySafely:(NSDictionary *)dictionary;
 -(void)setValuesForKeysWithDictionarySafely:(NSDictionary *)keyedValues;
++(id)convertDictionaryToObject:(NSDictionary *)dictionary objectType:(Class)objectType;
++(NSMutableArray *)convertDictionaryToObjects:(NSArray *)dictionaries objectType:(Class)objectType;
 
 @end
--- NSObject+Helper.m
+++ NSObject+Helper.m
@@ -1,13 +1,39 @@
 //
 //  NSObject+Helper.m
+//  TVGiDS.tv 1.0
 //
-//  Created by Bob de Graaf on 01-02-14.
-//  Copyright (c) 2014 GraafICT. All rights reserved.
+//  Created by Bob de Graaf on 08-07-13.
+//  Copyright (c) 2013 MobilePioneers. All rights reserved.
 //
 
 #import "NSObject+Helper.h"
 
 @implementation NSObject (Helper)
+
+-(id)initWithDictionarySafely:(NSDictionary *)dictionary
+{
+    self = [self init];
+    if(self) {
+        [self setValuesForKeysWithDictionarySafely:dictionary];
+    }
+    return self;
+}
+
++(id)convertDictionaryToObject:(NSDictionary *)dictionary objectType:(Class)objectType
+{
+    id object = [[objectType alloc] initWithDictionarySafely:dictionary];
+    return object;
+}
+
++(NSMutableArray *)convertDictionaryToObjects:(NSArray *)dictionaries objectType:(Class)objectType
+{
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    for(NSDictionary *dictionary in dictionaries) {
+        id object = [[objectType alloc] initWithDictionarySafely:dictionary];
+        [returnArray addObject:object];
+    }
+    return returnArray;
+}
 
 -(void)setValuesForKeysWithDictionarySafely:(NSDictionary *)keyedValues
 {
